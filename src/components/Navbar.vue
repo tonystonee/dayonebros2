a01cd07e119b51e4f0b84db7088a7742dc134cb7
Dionise <dionisea@intranet.nscl.msu.edu>
2019-01-21 16:14:26 -0500

    Navbar and Drawer

---
+++ src/components/Navbar.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+        <v-navigation-drawer
+        v-model="drawer"
+        fixed
+        clipped
+        app
+        >
+        <v-list dense>
+            <v-list-tile v-for="item in items" :key="item.text" @click="">
+                <v-list-tile-action>
+                    <v-icon>{{ item.icon }}</v-icon>
+                </v-list-tile-action>
+                <v-list-tile-content>
+                    <v-list-tile-title>
+                    {{ item.text }}
+                    </v-list-tile-title>
+                </v-list-tile-content>
+            </v-list-tile>
+        </v-list>
+        </v-navigation-drawer>
+        <v-toolbar
+        color="white"
+        dense
+        fixed
+        clipped-left
+        app
+        >
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+        <v-icon class="mx-3">fas fa-play-circle</v-icon>
+        <v-toolbar-title class="mr-5 align-center">
+            <span class="title">Day One Bros.com</span>
+        </v-toolbar-title>
+        </v-toolbar>
+    </div>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      drawer: null,
+      items: [
+        { icon: 'trending_up', text: 'News' },
+        { icon: 'fas fa-laugh-squint', text: 'Comedy' },
+        { icon: 'spa', text: 'Beauty' },
+        { icon: 'fas fa-football-ball', text: 'Sports' },
+        { icon: 'drive_eta', text: 'Auto' },
+        { icon: 'audiotrack', text: 'Music' },
+        { icon: 'fas fa-film', text: 'Film and Animation' },
+        { icon: 'videogame_asset', text: 'Gaming' },
+        { icon: 'pets', text: 'Pets' },
+        { icon: 'fas fa-flask', text: 'Science' },
+        { icon: 'fas fa-graduation-cap', text: 'Education' }
+      ],
+    }),
+    props: {
+      source: String
+    }
+  }
+</script>
+
+
---
+++ src/components/Player.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-layout>
+        <v-flex shrink>
+            
+        </v-flex>
+    </v-layout>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
