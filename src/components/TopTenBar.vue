bbf49560af25add7af408a3a1a16863c6386f5b0
Dionise <dionisea@intranet.nscl.msu.edu>
2019-01-23 17:02:43 -0500

    Seperate out components

---
+++ src/components/TopTenBar.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-card>
+        <v-toolbar
+        color="red"
+        class="white--text"
+        dense
+        >   
+        <h2 class="headline">Top 10</h2>
+        <v-spacer></v-spacer>
+        <v-icon color="white">whatshot</v-icon>
+        </v-toolbar>
+        <v-layout class="white pa-5" justify-center>
+            <v-progress-circular
+                indeterminate
+                color="primary"
+            ></v-progress-circular>
+        </v-layout>
+    </v-card>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,63 +5,49 @@
           <player/>  
         </v-flex>
         <v-flex xs12 md4>
-            <v-card>
-                <v-toolbar
-                color="red"
-                class="white--text"
-                dense
-                >   
-                <h2 class="headline">Top 10</h2>
-                <v-spacer></v-spacer>
-                <v-icon color="white">whatshot</v-icon>
-                </v-toolbar>
-                <v-layout class="white pa-5" justify-center>
-                    <v-progress-circular
-                        indeterminate
-                        color="primary"
-                    ></v-progress-circular>
-                </v-layout>
-            </v-card>
+            <top-ten-bar/>
         </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
-  import axios from 'axios';
-  import Player from '@/components/Player' 
-  export default {
-    data(){
-        return {
-            videos: null,
-            panel: [false],
-        };
-    },
-    components: {
-      Player,
-    },
-    computed: {
-      url(){
-        return `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=10&key=${this.$store.state.key}`;
-      },
-    },
-    methods: {
-        getVideos(url){
-            const self = this;
-            axios.get(url)
-            .then(function (response) {
-                // handle success
-                self.videos = response.data.items.map(item => item.snippet);
-            })
-            .catch(function (error) {
-                // handle error
-                console.log(error);
-            });
+    import axios from 'axios';
+    import Player from '@/components/Player' 
+    import TopTenBar from '@/components/TopTenBar' 
+    export default {
+        data(){
+            return {
+                videos: null,
+                panel: [false],
+            };
         },
-    },
-    mounted(){
-        //home page reel
-        this.getVideos(this.url);
-    },
-  }
+        components: {
+            Player,
+            TopTenBar,
+        },
+        computed: {
+            url(){
+                return `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=10&key=${this.$store.state.key}`;
+            },
+        },
+        methods: {
+            getVideos(url){
+                const self = this;
+                axios.get(url)
+                .then(function (response) {
+                    // handle success
+                    self.videos = response.data.items.map(item => item.snippet);
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                });
+            },
+        },
+        mounted(){
+            //home page reel
+            this.getVideos(this.url);
+        },
+    }
 </script>
