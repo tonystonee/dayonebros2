2a5cd63674afadb434a4e5e97c9836d53164bf9d
anthony <anthony.dionise@gmail.com>
2019-01-27 13:27:32 -0500

    top ten better

--- src/components/TopTenBar.vue
+++ src/components/TopTenBar.vue
@@ -1,26 +1,44 @@
+
 <template>
     <v-card class="top-ten">
-        <v-toolbar
-        color="red"
-        class="white--text"
-        dense
-        >   
-            <h2 class="headline">Top 10</h2>
+        <v-toolbar color="red" class="white--text">
+            <v-toolbar-title>Top 10</v-toolbar-title>
+
             <v-spacer></v-spacer>
+
             <v-icon color="white">whatshot</v-icon>
         </v-toolbar>
-        <div class="white px-2 pt-3 pb-4">
-            <v-card hover flat>
-                <v-layout row wrap class="pa-2">
-                    <v-flex xs4>
-                        <v-img src="https://i.ytimg.com/vi/1KiACA2ybTs/maxresdefault.jpg"></v-img>
-                    </v-flex>
-                    <v-flex  xs8 class="px-3">
-                        <h3 class="title">Title</h3>
-                    </v-flex>
-                </v-layout>
-            </v-card>
-        </div>
+
+        <v-list two-line>
+            <template v-for="(item, index) in items">
+            <v-subheader
+                v-if="item.header"
+                :key="item.header"
+            >
+            </v-subheader>
+
+            <v-divider
+                v-else-if="item.divider"
+                :inset="item.inset"
+                :key="index"
+            ></v-divider>
+
+            <v-list-tile
+                v-else
+                :key="item.title"
+                class="pa-2"
+            >
+                <v-img min-width="100px" left class="mr-2" :src="item.thumbnail"/>
+
+                <v-list-tile-content>
+                    <v-list-tile-title v-html="item.title"></v-list-tile-title>
+                    <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
+                </v-list-tile-content>
+            </v-list-tile>
+            </template>
+        </v-list>
+    </v-card>
+</template>
         <!-- <v-layout class="white pa-3" justify-center>
             <v-card>
                 <v-layout row wrap>
@@ -39,21 +57,39 @@
                 color="primary"
             ></v-progress-circular>
         </v-layout> -->
-    </v-card>
-</template>
+
 
 <script>
-export default {
-    name: 'TopTenbar',
-}
+  export default {
+    data () {
+      return {
+        items: [
+          {
+            thumbnail: 'https://i.ytimg.com/vi/1KiACA2ybTs/maxresdefault.jpg',
+            title: 'Brunch this weekend?',
+            subtitle: "<span class='text--primary'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?"
+          },
+          { divider: true, inset: true },
+          {
+            thumbnail: 'https://i.ytimg.com/vi/1KiACA2ybTs/maxresdefault.jpg',
+            title: 'Brunch this weekend?',
+            subtitle: "<span class='text--primary'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?"
+          },
+          { divider: true, inset: true },
+          {
+            thumbnail: 'https://i.ytimg.com/vi/1KiACA2ybTs/maxresdefault.jpg',
+            title: 'Brunch this weekend?',
+            subtitle: "<span class='text--primary'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?"
+          },
+        ]
+      }
+    }
+  }
 </script>
  <style lang="scss">
     .top-ten{
-        .v-card--hover:hover {
-            box-shadow: 
-                0px 5px 5px -3px rgba(0,0,0,0.2), 
-                0px 8px 10px 1px rgba(0,0,0,0.14), 
-                0px 3px 14px 2px rgba(0,0,0,0.12) !important;
+        .v-list--two-line .v-list__tile {
+            height: auto;
         }
     }
  </style>
