f9d6ff880707e79c228aaf7fda90dadf29d951b0
anthony <anthony.dionise@gmail.com>
2019-01-28 23:19:25 -0500

    Loading Transitions

--- src/components/TopTenBar.vue
+++ src/components/TopTenBar.vue
@@ -1,48 +1,50 @@
 
 <template>
 <div>
-    <v-card class="top-ten" v-if="videoList">
-        <v-toolbar color="red" class="white--text">
-            <v-toolbar-title>Top 10</v-toolbar-title>
+    <v-expand-transition>
+        <v-card class="top-ten" v-if="videoList">
+            <v-toolbar color="red" class="white--text">
+                <v-toolbar-title>Top 10</v-toolbar-title>
 
-            <v-spacer></v-spacer>
+                <v-spacer></v-spacer>
 
-            <v-icon color="white">whatshot</v-icon>
-        </v-toolbar>
+                <v-icon color="white">whatshot</v-icon>
+            </v-toolbar>
 
-        <v-list two-line>
-            <template  v-for="(item, index) in videoList">
-                <v-tooltip hover :key="index" left>
-                    <div slot="activator">
-                        <v-list-tile
-                            :key="item.title"
-                            class="pa-2"
-                        >
-                            <v-img min-width="100px" left class="mr-2" :src="item.thumbnails.medium.url"/>
+            <v-list two-line>
+                <template  v-for="(item, index) in videoList">
+                    <v-tooltip hover :key="index" left>
+                        <div slot="activator">
+                            <v-list-tile
+                                :key="item.title"
+                                class="pa-2"
+                            >
+                                <v-img min-width="100px" left class="mr-2" :src="item.thumbnails.medium.url"/>
 
-                            <v-list-tile-content>
-                                <v-list-tile-title v-html="item.title"></v-list-tile-title>
-                                <v-list-tile-sub-title v-html="item.description"></v-list-tile-sub-title>
-                            </v-list-tile-content>
-                        </v-list-tile>
-                    </div>
-                    <span>
-                        <h3 class="subtitle">
-                            {{item.title}}
-                        </h3>
-                    </span>
-                </v-tooltip>
-            </template>
-        </v-list>
-    </v-card>
-    <v-card v-else>
-        <v-layout class="white pa-3" justify-center>
-                <v-progress-circular
-                    class="py-5"
-                    indeterminate
-                    color="primary"
-                ></v-progress-circular>
-        </v-layout>
+                                <v-list-tile-content>
+                                    <v-list-tile-title v-html="item.title"></v-list-tile-title>
+                                    <v-list-tile-sub-title v-html="item.description"></v-list-tile-sub-title>
+                                </v-list-tile-content>
+                            </v-list-tile>
+                        </div>
+                        <span>
+                            <h3 class="subtitle">
+                                {{item.title}}
+                            </h3>
+                        </span>
+                    </v-tooltip>
+                </template>
+            </v-list>
+        </v-card>
+    </v-expand-transition>
+    <v-card v-if="!videoList">
+            <v-layout class="white pa-3" justify-center>
+                    <v-progress-circular
+                        class="py-5"
+                        indeterminate
+                        color="primary"
+                    ></v-progress-circular>
+            </v-layout>
     </v-card>
 </div>
 </template>
--- src/components/VideoDetails.vue
+++ src/components/VideoDetails.vue
@@ -1,34 +1,36 @@
 <template>
-    <v-expansion-panel v-if="video"
-        v-model="panel"
-        expand
-    >
-        <v-layout>
-            <h2 class="pa-3">{{video.title}}</h2>
-        </v-layout>
-        <v-expansion-panel-content>
-            <div slot="header" class="font-weight-bold">
-                <span v-if="!panel[0]">Show More</span>
-                <span v-else>Close 
-                    <v-icon small right>close</v-icon>
-                </span>
-            </div>
-            <v-card>
-                <v-card-text>
-                    {{video.description}}
-                </v-card-text>
-            </v-card>
-        </v-expansion-panel-content>
-    </v-expansion-panel>
-    <v-card v-else>
-        <v-layout class="white pa-3" justify-center>
-                <v-progress-circular
-                    class="py-5"
-                    indeterminate
-                    color="primary"
-                ></v-progress-circular>
-        </v-layout>
-    </v-card>
+<div>
+    <v-scale-transition>
+            <v-expansion-panel v-if="video"
+                v-model="panel"
+                expand
+            >
+                <v-layout>
+                    <h2 class="pa-3">{{video.title}}</h2>
+                </v-layout>
+                <v-expansion-panel-content>
+                    <div slot="header" class="font-weight-bold">
+                        <span v-if="!panel[0]">Show More</span>
+                        <span v-else>Close 
+                            <v-icon small right>close</v-icon>
+                        </span>
+                    </div>
+                    <v-card>
+                        <v-card-text>
+                            {{video.description}}
+                        </v-card-text>
+                    </v-card>
+                </v-expansion-panel-content>
+            </v-expansion-panel>
+    </v-scale-transition>
+    <v-layout v-if="!video" class="white pa-3" justify-center>
+            <v-progress-circular
+                class="py-5"
+                indeterminate
+                color="primary"
+            ></v-progress-circular>
+    </v-layout>
+</div>
 </template>
 
 <script>
